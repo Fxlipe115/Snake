6db76dbe8581ec1b7fb24c53669c06eab0a0df90
Felipe Graeff <felipegraeff@hotmail.com>
2016-05-11 15:08:06 -0300

    Update grf_snake_lib.h

--- grf_snake_lib.h
+++ grf_snake_lib.h
@@ -1,9 +1,15 @@
 #ifndef GRF_SNAKE_LIB_H
 # define GRF_SNAKE_LIB_H
 
+//Macros de direção para a função moveSnake()
+#define RIGHT 0
+#define UP 90
+#define LEFT 180
+#define DOWN 270
+
 typedef struct snake Snake;
 
-Snake* newSnake();
+Snake* newSnake(int size);
 
 void destroySnake(Snake *head);
 
@@ -12,5 +18,7 @@ void increaseSnake(Snake *head);
 void decreaseSnake(Snake *head);
 
 int getSnakeSize(Snake *head);
+
+void moveSnake(Snake *snake,int dir);
 
 #endif
