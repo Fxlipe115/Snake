c9b7e4304068c598eba937edaf00f21ea2890e23
Fxlipe115 <felipegraeff@hotmail.com>
2016-05-17 12:24:40 -0300

    mores libs

--- src/grf_draw_lib.c
+++ src/grf_draw_lib.c
@@ -1,3 +1,12 @@
-
 #include <stdio.h>
+#include <"../include/grf_snake_lib.h">
 
+//Carrega o mapa do arquivo "filename" e o retorna como uma string junto com a altura e largura do mesmo
+char* loadMap(char* filename,int *width,int *height){
+	//TODO
+}
+
+//Imprime a tela com todos os elementos nela
+void refreshScreen(){
+	//TODO
+}
