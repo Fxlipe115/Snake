c166ce4c4233149089d2887eb32b418479c0694c
Fxlipe115 <felipegraeff@hotmail.com>
2016-05-18 13:55:09 -0300

    Lib correction

--- src/grf_draw_lib.c
+++ src/grf_draw_lib.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include <"../include/grf_snake_lib.h">
+//#include <"grf_snake_lib.h">
 
 //Carrega o mapa do arquivo "filename" e o retorna como uma string junto com a altura e largura do mesmo
 char* loadMap(char* filename,int *width,int *height){
 	//TODO
+	return NULL;
 }
 
 //Imprime a tela com todos os elementos nela
